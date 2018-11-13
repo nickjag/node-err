1fb20d0ca493d0d1ccc5a39cfb830526892a50e5
Alexander Sapountzis <alex@stashable.com>
2018-11-13 09:57:46 -0500

    Add tests for getStatus()

--- index.test.js
+++ index.test.js
@@ -7,6 +7,27 @@ describe('#repeat', () => {
   }
 
   it('throws an error', () => {
-      assert.throws(() => nodeErr.repeat(error()), /^Error: I AM ERROR$/);
+    assert.throws(() => nodeErr.repeat(error()), /^Error: I AM ERROR$/);
   });
 });
+
+describe('#getStatus', () => {
+  it('returns an http status', () => {
+    try {
+      throw new Error('I AM ERROR');
+    } catch (err) {
+      assert.equal(nodeErr.getStatus(err), 500);
+    }
+  });
+
+  it('should allow configuration of http status', () => {
+    nodeErr.setup({ status: 401 });
+
+    try {
+      throw new Error('I AM ERROR');
+    } catch (err) {
+      assert.equal(nodeErr.getStatus(err), 401);
+    }
+  });
+});
+
